ab59a4186d844f861368ebbc74de3c06dc446975
mkvalentine <megan.valentine@yale.edu>
2016-05-14 22:09:52 -0400

    Added files for Through prototype in unity

---
+++ Through_Prototype_Unity/Scripts/LayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LayerController : MonoBehaviour {
+
+	int topLayer = 15;
+	int bottomLayer = 10;
+	int currentLayer = 15;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Z) && currentLayer > bottomLayer) {
+			hideDimension(currentLayer--);
+		}
+		if (Input.GetKeyDown(KeyCode.X) && currentLayer < topLayer) {
+			showDimension(++currentLayer);
+		}
+	}
+
+	void hideDimension(int layerToHide) {
+		Camera.main.cullingMask &= ~(1 << layerToHide);
+
+	}
+	
+	void showDimension(int layerToShow) {
+		Camera.main.cullingMask |= (1 << layerToShow);
+	}
+}
